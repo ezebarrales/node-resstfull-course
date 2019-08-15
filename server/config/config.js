58bfd5cdf602871adb8c7d43e1ed2dce227b0d0a
Ezequiel Barrales <ezequiel@ezequielbarrales.com>
2019-08-15 20:26:15 -0300

    Seguridad DB Prod Connection

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,6 @@ switch(process.env.ENV) {
         process.env.DB_CONNECTION = 'mongodb://localhost:27017/cafe';
         break;
     case 'prod':
-        process.env.DB_CONNECTION = 'mongodb+srv://root:75966957@cluster0-lhwh7.mongodb.net/test';
+        process.env.DB_CONNECTION = process.env.HEROKU_DB_URI;
         break;
 }
